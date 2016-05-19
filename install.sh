21dd4b572b6ee5082dd812e5efa1ffa5beb1657f
Ben Maynard <bmaynard@logicworks.net>
2016-05-19 10:50:07 -0400

    Updating

--- install.sh
+++ install.sh
@@ -18,6 +18,9 @@ mkdir -p ~/.vim/undo
 
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
+echo "Waiting for oh-my-zsh to install"
+sleep 10
+
 cp tmux.conf.sane ~/.tmux.conf
 cp vimrc.sane ~/.vimrc
 cp zshrc.sane ~/.zshrc
